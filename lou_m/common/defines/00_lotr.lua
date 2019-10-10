46967b6464b8148b8bbff55958d8b6756f4772ff
ThePrograminator <christian_skyder@hotmail.com>
2019-10-10 19:32:03 +0200

    first commit

---
+++ lou_m/common/defines/00_lotr.lua
@@ -0,0 +1,60 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.MAX_CLIENT_STATES = 20
+
+NDefines.NGame.START_DATE = "2758.1.1"
+NDefines.NGame.END_DATE = "3500.1.1"
+NDefines.NGame.MAX_RANDOM_NEW_WORLD = "3500.1.1"
+
+NDefines.NCountry.STARTING_ARMY_SIZE = 0.75
+
+NDefines.NCountry.MONARCH_DEATH = 0
+NDefines.NCountry.MAX_CROWN_COLONIES = 999
+
+NDefines.NCountry.POWER_MAX = 2800	
+NDefines.NCountry.ABANDON_IDEAGROUP_REFUND = 1.00
+
+NDefines.NFrontend.CAMERA_LOOKAT_X = 1100.0
+NDefines.NFrontend.CAMERA_LOOKAT_Z = 1800.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 2787.0
+NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 1845.0
+NDefines.NFrontend.CAMERA_START_X = 0.0
+NDefines.NFrontend.CAMERA_START_Z = 0.0
+NDefines.NFrontend.CAMERA_END_X = 1453.0
+NDefines.NFrontend.CAMERA_END_Z = 0.0
+NDefines.NFrontend.FRONTEND_POS_X = 0.0
+NDefines.NFrontend.FRONTEND_POS_Z = 0.0
+NDefines.NFrontend.FRONTEND_LOOK_X = 1100.0
+NDefines.NFrontend.FRONTEND_LOOK_Z = 800.0
+
+NDefines.NCountry.INSTITUTION_BONUS_FROM_IMP_DEVELOPMENT = 0
+NDefines.NCountry.INSTITUTION_CAP_IMP_DEVELOPMENT = 1
+NDefines.NCountry.INSTITUTION_BASE_IMP_DEVELOPMENT = 1
+	
+NDefines.NCountry.EMBRACE_INSTITUTION_COST = 1000.0				-- 1000.0 per development (autonomy modified)
+
+--NDefines.NFrontend.SETTINGS_POS_X = 2787.0
+--NDefines.NFrontend.SETTINGS_POS_Z = 1228.0
+--NDefines.NFrontend.SETTINGS_LOOK_X = 2787.0
+--NDefines.NFrontend.SETTINGS_LOOK_Z = 1499.0
+--NDefines.NFrontend.MP_OPTIONS_POS_X = 2787.0
+--NDefines.NFrontend.MP_OPTIONS_POS_Z = 1398.0
+--NDefines.NFrontend.MP_OPTIONS_LOOK_X = 2787.0
+--NDefines.NFrontend.MP_OPTIONS_LOOK_Z = 1459.0
+--NDefines.NFrontend.TUTORIAL_POS_X = 2724.0
+--NDefines.NFrontend.TUTORIAL_POS_Z = 1654.0
+--NDefines.NFrontend.TUTORIAL_LOOK_X = 2710.0
+--NDefines.NFrontend.TUTORIAL_LOOK_Z = 1792.0
+--NDefines.NFrontend.CONTENT_POS_X = 2787.0
+--NDefines.NFrontend.CONTENT_POS_Z = 1642.0
+--NDefines.NFrontend.CONTENT_LOOK_X = 2787.0
+--NDefines.NFrontend.CONTENT_LOOK_Z = 1845.0
+--NDefines.NFrontend.CREDITS_POS_X = 2911.0
+--NDefines.NFrontend.CREDITS_POS_Z = 1824.0
+--NDefines.NFrontend.CREDITS_LOOK_X = 2916.0
+--NDefines.NFrontend.CREDITS_LOOK_Z = 2027.0
+
+NDefines.NDiplomacy.END_OF_CRUSADES = 9999
+
+--NDefines.NCountry.ESTATE_PROVINCE_POWER = 3.0
+--NDefines.NCountry.ESTATE_PROVINCE_POWER_MAX = 50
