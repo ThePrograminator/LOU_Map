5c9060ff86957362f665bc9253421cb902cb73df
ThePrograminator <christian_skyder@hotmail.com>
2020-07-12 18:18:21 +0200

    Version 1.30.3.0

--- lou_m/common/defines/00_lotr.lua
+++ lou_m/common/defines/00_lotr.lua
@@ -14,19 +14,6 @@ NDefines.NCountry.MAX_CROWN_COLONIES = 999
 NDefines.NCountry.POWER_MAX = 2800	
 NDefines.NCountry.ABANDON_IDEAGROUP_REFUND = 1.00
 
-NDefines.NFrontend.CAMERA_LOOKAT_X = 1100.0
-NDefines.NFrontend.CAMERA_LOOKAT_Z = 1800.0
-NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_X = 2787.0
-NDefines.NFrontend.CAMERA_LOOKAT_SETTINGS_Z = 1845.0
-NDefines.NFrontend.CAMERA_START_X = 0.0
-NDefines.NFrontend.CAMERA_START_Z = 0.0
-NDefines.NFrontend.CAMERA_END_X = 1453.0
-NDefines.NFrontend.CAMERA_END_Z = 0.0
-NDefines.NFrontend.FRONTEND_POS_X = 0.0
-NDefines.NFrontend.FRONTEND_POS_Z = 0.0
-NDefines.NFrontend.FRONTEND_LOOK_X = 1100.0
-NDefines.NFrontend.FRONTEND_LOOK_Z = 800.0
-
 NDefines.NCountry.INSTITUTION_BONUS_FROM_IMP_DEVELOPMENT = 0
 NDefines.NCountry.INSTITUTION_CAP_IMP_DEVELOPMENT = 1
 NDefines.NCountry.INSTITUTION_BASE_IMP_DEVELOPMENT = 1
@@ -54,7 +41,7 @@ NDefines.NCountry.EMBRACE_INSTITUTION_COST = 1000.0				-- 1000.0 per development
 --NDefines.NFrontend.CREDITS_LOOK_X = 2916.0
 --NDefines.NFrontend.CREDITS_LOOK_Z = 2027.0
 
-NDefines.NDiplomacy.END_OF_CRUSADES = 9999
+--NDefines.NDiplomacy.END_OF_CRUSADES = 9999
 
 --NDefines.NCountry.ESTATE_PROVINCE_POWER = 3.0
 --NDefines.NCountry.ESTATE_PROVINCE_POWER_MAX = 50
